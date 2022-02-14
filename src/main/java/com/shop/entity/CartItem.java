332a69aa7057dc8bab8ebbace6f4c5b0dedbc619
SittingDeveloper <rlaaudtlr145@gmail.com>
2022-02-14 15:10:47 +0900

    장바구니 / 주문상품 추가

---
+++ src/main/java/com/shop/entity/CartItem.java
@@ -0,0 +1,28 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "cart_item")
+public class CartItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    // 하나의 장바구니에는 여러 개의 상품을 담을 수 있으므로 @ManyToOne
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item; // 장바구니에 담을 상품의 정보를 알아야 하므로 상품 엔티티를 매핑.
+
+    private int count; // 같은 상품을 장바구니에 몇개 담을지 저장
+}
---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,46 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders") // 정렬에 사용하는 order 라는 키워드가 이미 존재하기 때문에 orders 로 변경
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue
+    @JoinColumn(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member; // 한명의 회원은 여러번 주문을 할 수 있으므로 주문 엔티티에서 다대일 단방향 매핑
+
+    private LocalDateTime orderDate; // 주문일
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; // 주문 상태
+
+    /*
+    주문 상품 엔티티와 일대다 매핑
+    외래키(order_id)가 order_item 테이블에 있으므로 연관 관계의 주인은 OrderItem Entity 이다.
+    Order 엔티티가 주인이 아니므로 "mappedBy" 속성으로 연관 관계의 주인을 설정한다.
+    속성의 값으로 "order" 를 적어준 이유는 OrderItem 에 있는 Order 에 의해 관리된다는 의미로 해석하면 된다.
+    즉, 연관 관계의 주인의 필드인 order 를 mappedBy 의 값으로 세팅하면 된다.
+
+    하나의 주문이 여러개의 주문 상품을 갖으므로 List 자료형을 사용해서 매핑을 한다.
+     */
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
