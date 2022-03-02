feecdbb56c9ce4f65f229b4c4b53b49e4bbef344
SittingDeveloper <rlaaudtlr145@gmail.com>
2022-03-02 17:39:34 +0900

    주문 이력 페이지 오류 존재

--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -10,4 +10,7 @@ public interface ItemImgRepository extends JpaRepository<ItemImg, Long > {
     // 이미지가 잘 저장됬는지 테스트 코드를 작성하기 위한 인터페이스
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 
+    // 상품의 대표 이미지를 찾는 쿼리 메소드
+    ItemImg findByItemIdAndRepimgYn(Long itemId, String repimgYn);
+
 }
--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -1,14 +1,17 @@
 package com.shop.service;
 
 import com.shop.dto.OrderDto;
-import com.shop.entity.Item;
-import com.shop.entity.Member;
-import com.shop.entity.Order;
-import com.shop.entity.OrderItem;
+import com.shop.dto.OrderHistDto;
+import com.shop.dto.OrderItemDto;
+import com.shop.entity.*;
+import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
 import com.shop.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,24 +27,58 @@ public class OrderService { // 주문 로직
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto, String email) {
         Item item = itemRepository.findById(orderDto.getItemId()) // 주문할 상품을 조회
-                .orElseThrow(EntityNotFoundException::new); 
+                .orElseThrow(EntityNotFoundException::new);
         Member member = memberRepository.findByEmail(email); // 현재 로그인한 회원의 이메일 정보를 이용해서 회원 정보를 조회
 
         List<OrderItem> orderItemList = new ArrayList<>();
-        
+
         // 주문할 상품 엔티티와 주문 수량을 이용하여 주문 상품 엔티티 생성
-        OrderItem orderItem = OrderItem.createOrderItem(item, orderDto.getCount()); 
-        
+        OrderItem orderItem = OrderItem.createOrderItem(item, orderDto.getCount());
+
         orderItemList.add(orderItem);
 
         // 회원 정보와 주문할 상품 리스트 정보를 이용하여 주문 엔티티를 생성
-        Order order = Order.createOrder(member, orderItemList); 
+        Order order = Order.createOrder(member, orderItemList);
         orderRepository.save(order); // 생성한 주문 엔티티를 저장
 
         return order.getId();
     }
 
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+
+        // 유저의 아이디와 페이징 조건을 이용하여 주문 목록을 조회
+        List<Order> orders = orderRepository.findOrders(email, pageable);
+
+        // 유저의 주문 총 개수를 구함
+        Long totalCount = orderRepository.countOrder(email);
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        // 주문 리스트를 순회하면서 구매 이력 페이지에 전달할 DTO 생성
+        for (Order order : orders) {
+
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+
+            for (OrderItem orderItem : orderItems) {
+
+                // 주문한 상품의 대표 이미지를 조회
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepimgYn(orderItem.getItem().getId(), "Y");
+
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        // 페이지 구현 객체를 생성하여 반환
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
+
+    }
 }
